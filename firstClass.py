4057294451cd805bc00ca8554446fcd55d54458d
juraaj <juraj.kirka@gmail.co>
2016-11-17 18:16:52 +0100

    3rd commit

--- firstClass.py
+++ firstClass.py
@@ -5,6 +5,11 @@ class Point:
         self.x = x
         self.y = y
 
+    def distance_from_origin(self):
+        return (self.x**2 + self.y**2)**0.5
+
+    def distance_from_another(self, another):
+        return ((self.x - another.x)** 2 + (self.y - another.y) ** 2) ** 0.5
 
 
 p = Point()
