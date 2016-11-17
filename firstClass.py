3ad71e88c2d2667158a13f6da48867c9d41710e3
juraaj <juraj.kirka@gmail.co>
2016-11-17 18:04:02 +0100

    Second commit

---
+++ firstClass.py
@@ -0,0 +1,13 @@
+class Point:
+    __secretAttr = 0
+
+    def __init__(self,x=7,y=12):
+        self.x = x
+        self.y = y
+
+
+
+p = Point()
+
+print(str(p.x) + " " + str(p.y))
+print(p._Point__secretAttr)
